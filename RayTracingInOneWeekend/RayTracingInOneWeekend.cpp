fee4266a6ecc8e5e865164a49850dbf38f69def6
Konstantin Anthony Romanov <ant@theixo.com>
2023-01-09 20:21:28 -0600

    Initial CMake project

---
+++ RayTracingInOneWeekend/RayTracingInOneWeekend.cpp
@@ -0,0 +1,12 @@
+﻿// RayTracingInOneWeekend.cpp : Defines the entry point for the application.
+//
+
+#include "RayTracingInOneWeekend.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
